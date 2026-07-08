27ea67701b7eb671247f3f239a971fb64ed33826
xiaofei <xiaofei00152@gmail.com>
2026-07-08 11:43:46 +0800

    更新博客

--- docs/.vuepress/notes.ts
+++ docs/.vuepress/notes.ts
@@ -4,8 +4,8 @@ import { defineCollection } from 'vuepress-theme-plume'
 export const notes = [
     {
         type: 'post', // 替代原博客功能
-        dir: 'preview', // 指向 docs/blog 目录
-        // title: '博客' // 集合显示名称
+        dir: 'blog', // 指向 docs/blog 目录
+        title: '博客', // 集合显示名称
         postList: true, // 是否启用文章列表页
         tags: true, // 是否启用标签页
         archives: true, // 是否启用归档页
@@ -453,6 +453,4 @@ export const notes = [
         ],
         sidebarCollapsed: true, // 是否默认折叠侧边栏
     },
-    // 博客
-  { type: 'post', dir: '/blog/', link: '/blog/', title: 'Blog' },
 ]
